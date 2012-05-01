c73049dcb9907481de0d96ca341f62ff949ef2d6
Jer Wilson <realaddress@legiturl.com>
2012-04-30 23:08:38 -0700

    Figured out vector math for mimicking hammer's "Fit" option for textures

---
+++ texfit-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+awk -F '"( ")?' \
+ '{ if($2=="plane") pln=$3; if($2=="uaxis") system("./texfit.py \"" pln "\""); else if($2!="vaxis") print $0 }' \
+  mapsrc/superjer-card.vmf \
+ >mapsrc/superjer-card-fixed.vmf
